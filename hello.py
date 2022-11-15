c244c35543f2285497816ba6509074f28e06a402
Rishi <rishikumar60708090@gmail.com>
2022-11-15 18:20:50 +0530

    this is

---
+++ hello.py
@@ -0,0 +1,21 @@
+# a = 1 
+# b = 2 
+# for i in range(b):
+#     for j in range(a):
+#         print(i , j)
+
+
+
+
+# a = input("enter your name")
+# b = ["rahul" , "satish"  , "mukesh"]
+# if a == b:
+#     print("right name are chosed")
+
+# else:
+#     print("you are chosed by wrong name ")
+
+
+
+import tensorflow as tf
+tf.__version__
